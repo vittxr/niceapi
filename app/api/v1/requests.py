051565a646f25329fd002b304657ea8b678ed203
Vit <vitor.roberto3022@gmail.com>
2022-07-02 15:05:57 -0300

    add put request

--- app/api/v1/requests.py
+++ app/api/v1/requests.py
@@ -1,9 +1,10 @@
 ##Esse arquivo faz as requisições do usuário, requisitadas no front-end
-from xml.sax.handler import feature_external_ges
 import requests 
 from app.api.v1 import api_v1
 from flask import jsonify, redirect, render_template, request, url_for, json
 from bs4 import BeautifulSoup #pip install beautifulsoap4
+from app.models import User
+from app import db
 
 @api_v1.route("/main", methods=["GET", "POST"])
 def main(): 
@@ -22,7 +23,9 @@ def main():
         case ('POST'):
            return redirect(url_for("api_v1.post_apidata", url=url, name=name, cpf=cpf, email=email, password=password))
         case ('PUT'):
-           return redirect(url_for("api_v1.put_apidata", url=url, name=name, cpf=cpf, email=email, password=password))
+           userToBeAltered = request.args.get('selected-user')
+           print(userToBeAltered)
+           return redirect(url_for("api_v1.put_apidata", url=url, name=name, cpf=cpf, email=email, password=password, userToBeAltered=userToBeAltered))
         case ('DELETE'):
            return redirect(url_for("api_v1.delete_apidata", url=url, name=name, cpf=cpf, email=email, password=password))
     
@@ -30,57 +33,88 @@ def main():
 @api_v1.route("/get_apidata")
 def get_apidata():
     global res 
-    res = '' 
+    res = ""
+    if request.args.get('url'):
+        url = request.args.get('url')
+        full_url = getUrl(url)
+        res = requests.get(full_url)
 
-    if request.args: 
-        default_url = """
-                        <label for="url" class="search-apiData__ipt-url-predefined" name="default-url">
-                            https://niceeapi.herokuapp.com/api/v1/
-                        </label>"
-                    """
-        
+    try:
+        res_json = res.json()
+    except: 
+        res_json = "an error occurred :/"
 
-        url = BeautifulSoup(default_url, features="html.parser").label.string.replace(' ', '').replace('\n', '') + request.args.get('url')
-        #BeatifulSoup é uma classe que retorna o conteúdo do html
-        #os replaces usados são para remover espaços em branco e quebras de linha, deixando a url em um formato adequado.
-        res = requests.get(url)
+    if request.args.get('request_detail'): 
+        res_json = json.dumps(request.args.get('request_detail'))
+           #detalhe da requisição POST, PUT e delete. Exibe uma mensagem falando se ela foi um sucesso ou não
 
-    return render_template('api_manager.html', res=res)
+    return render_template('api_manager.html', res_json=res_json)
 
 
 @api_v1.route("/post_apidata", methods=["GET", "POST"])
 def post_apidata():
-    if request.args: 
-        default_url = """
-                        <label for="url" class="search-apiData__ipt-url-predefined" name="default-url">
-                            https://niceeapi.herokuapp.com/api/v1/
-                        </label>"
-                    """
-        url = BeautifulSoup(default_url, features="html.parser").label.string.replace(' ', '').replace('\n', '') + request.args.get('url')
+    data = dataToDict( 
+        name = request.args.get('name'),
+        cpf = request.args.get('cpf'),
+        email = request.args.get('email'),
+        password = request.args.get('password'),
+        ) 
 
-        name = request.args.get('name')
-        cpf = request.args.get('cpf')
-        email = request.args.get('email')
-        password = request.args.get('password')
+    url = request.args.get('url')
+    full_url = getUrl(url)
 
-        print(cpf, email)
-
-        data = {
-             "name": f"{name}",
-             "cpf": f"{cpf}", 
-             "email": f"{email}", 
-             "password": f"{password}"
-        }
-        
-        res = requests.post(url, data=json.dumps(data))
+    if data and url:
+        res = requests.post(full_url, data=json.dumps(data))
           #dumps serializa um dicionário p/ json.
+        return redirect(url_for("api_v1.get_apidata", request_detail="request_response -> success"))
 
-    return redirect(url_for("api_v1.get_apidata"))
+    return redirect(url_for("api_v1.get_apidata", request_detail="request_response -> error"))
 
 @api_v1.route("/put_apidata")
 def put_apidata():
-    pass
+    userToBeAltered = request.args.get('userToBeAltered')
+    print(f"userTobeAltered => {userToBeAltered}")
+
+    user = User.query.filter_by(email=userToBeAltered).first()
+    print(user)
+    
+    if user:
+        user.name = request.args.get("name")
+        user.cpf = request.args.get("cpf")
+        user.email = request.args.get("email")
+        user.password = request.args.get("password")
+
+        print(user)
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for("api_v1.get_apidata", resquest_detail = "request_response -> success"))
+
+    return redirect(url_for("api_v1.get_apidata", resquest_detail = "request_response -> error"))
+    
 
 @api_v1.route("/delete_apidata")
 def delete_apidata():
     pass
+
+##funções auxiliares (usados nas rotas:
+def dataToDict(name, cpf, email, password):
+    data = {
+        "name": f"{name}",
+        "cpf": f"{cpf}", 
+        "email": f"{email}", 
+        "password": f"{password}"
+    }
+    return data
+
+def getUrl(url):
+    default_url = """
+                        <label for="url" class="search-apiData__ipt-url-predefined" name="default-url">
+                            https://niceeapi.herokuapp.com/api/v1/
+                        </label>"
+                  """
+    full_url = BeautifulSoup(default_url, features="html.parser").label.string.replace(' ', '').replace('\n', '') + url
+        #BeatifulSoup é uma classe que retorna o conteúdo do html
+        #os replaces usados são para remover espaços em branco e quebras de linha, deixando a url em um formato adequado.
+    return full_url
--- app/static/js/api_manager/type-request.js
+++ app/static/js/api_manager/type-request.js
@@ -1,20 +1,42 @@
-//esse arquivo exibe os campos, de acordo com o tipo de requisição. Se for post, put ou delete, aparece os campos para o usuário preencer. 
-var select = document.querySelector(".manager-api__type-request")
-var request_fields = document.querySelector(".manager-api__type-request-fields")
-var form = document.querySelector(".manager-api__box form")
+//esse arquivo exibe os campos, de acordo com o tipo de requisição. Se for post, put ou delete, aparece os campos para o usuário preencher. 
+var select = document.querySelector(".manager-api__type-request");
+var request_fields__box = document.querySelector(".manager-api__request-fields");
+var request_fields__input = document.querySelectorAll(".manager-api__request-fields input");
+var form = document.querySelector(".manager-api__box form");
+var select_users = document.querySelector(".manager-api__select-user");
 
 select.addEventListener('change', ChangeTheDisplayOfRequestFields)
 function ChangeTheDisplayOfRequestFields() {
     let option_selected = select.options[select.selectedIndex].text
     switch (option_selected) {
         case "GET":
-            request_fields.style.display = 'none'
-            break
-        default: 
-            //Para todos os outros caso, request_fields deve ser display: flex. Então, basta usar o default.
-            request_fields.style.display = 'flex'
-            form.setAttribute("methods", "post")
-            break    
+            request_fields__box.style.display = 'none'
+            request_fields__input.forEach((e, index) => {
+                request_fields__input[index].removeAttribute("required", "")
+            });
+            select_users.style.display = 'none'
+            break;  
+
+        case "POST": 
+            select_users.style.display = 'none'
+            request_fields__box.style.display = 'flex';
+            form.setAttribute("methods", "post");
+            request_fields__input.forEach((e, index) => {
+                request_fields__input[index].setAttribute("required", "")
+                  //deixa os campos obrigatórios a ser preenchido.
+            });
+            break;
+
+        case "PUT":
+        case "DELETE":
+            select_users.style.display = 'block'  
+            request_fields__box.style.display = 'flex';
+            form.setAttribute("methods", "post");
+            request_fields__input.forEach((e, index) => {
+                request_fields__input[index].setAttribute("required", "")
+                  //deixa os campos obrigatórios a ser preenchido.
+            });
+            break;    
     }
 }
 
