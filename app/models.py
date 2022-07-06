10070948b18cd02c05115e0e9d94a6e0d93d8334
Vit <vitor.roberto3022@gmail.com>
2022-07-06 17:59:52 -0300

    fix models and add require attribute in input fields

--- app/models.py
+++ app/models.py
@@ -32,7 +32,6 @@ class User(db.Model, UserMixin):
         return {
             "id":self.id, 
             "name":self.name,
-            "cpf":self.cpf,
             "email":self.email,
         }        
     
