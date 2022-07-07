413731735b9404a8ea106b814e7462144b8d6f91
Vit <vitor.roberto3022@gmail.com>
2022-07-07 12:04:35 -0300

    refactoring

--- app/api/v1/requests.py
+++ app/api/v1/requests.py
@@ -1,13 +1,11 @@
 ##Esse arquivo faz as requisições do usuário, requisitadas no front-end
-from threading import Timer
 from flask_login import login_required, current_user
 import requests 
 from app.api.v1 import api_v1
 from flask import redirect, render_template, request, url_for, json
-from bs4 import BeautifulSoup #pip install beautifulsoap4
 from app.models import User
 from app import db
-from datetime import datetime
+from ..utils import getUrl, dataToDict, userCanMakeRequest
 
 @api_v1.route("/main", methods=["GET", "POST"])
 def main(): 
@@ -126,56 +124,3 @@ def delete_apidata():
        
 
     return redirect(url_for("api_v1.get_apidata", request_detail = "request_response -> error"))
-
-##funções auxiliares (usados nas rotas:
-def dataToDict(name, email, password):
-    data = {
-        "name": f"{name}",
-        "email": f"{email}", 
-        "password": f"{password}"
-    }
-    return data
-
-def getUrl(url):
-    default_url = """
-                        <label for="url" class="search-apiData__ipt-url-predefined" name="default-url">
-                            https://niceeapi.herokuapp.com/api/v1/
-                        </label>"
-                  """
-    full_url = BeautifulSoup(default_url, features="html.parser").label.string.replace(' ', '').replace('\n', '') + url
-        #BeatifulSoup é uma classe que retorna o conteúdo do html
-        #os replaces usados são para remover espaços em branco e quebras de linha, deixando a url em um formato adequado.
-    return full_url
-
-def userCanMakeRequest():
-    error=""
-    #essa função verifica se o usuário pode fazer requisições ainda. Além disso, ela aumenta o valor do número de requisições no banco de dados.
-    if current_user.is_authenticated: 
-       #user = User.query.filter_by(email=current_user.email).first()
-       if current_user.requests_number == 10:
-            hourTimerWillEnd = ResetRequestsNumberAfterOneDay()
-            error = f"request_response -> Número máximo de requisições atingido. Para fazê-las novamente, é preciso esperar 1 dia. Você poderá fazer mais requisições amanhã, a partir das {hourTimerWillEnd.time().strftime('%H:%M')} - GMT-0 "
-            return error
-
-       current_user.requests_number = current_user.requests_number + 1
-       db.session.add(current_user)
-       db.session.commit()
-       return True 
-         #Se o retorno da função for true, o usuário pode fazer a requisição 
-
-    error = "request_response -> é preciso estar logado para fazer esse tipo de requisição :/"
-    return error
-
-def ResetRequestsNumberAfterOneDay(): 
-    usr = User.query.filter_by(email = current_user.email).first() 
-    hourTimerWillEnd = datetime.now()
-
-    t = Timer(86200.0, resetRequestsNumberInDb, (usr))
-    t.start() 
-
-    return hourTimerWillEnd
-
-def resetRequestsNumberInDb(usr): 
-    usr.requests_number = 0
-    db.session.add(usr)
-    db.session.commit()
