fed59c184d1a7be097fbd4e07fe43b7f43505177
Vit <vitor.roberto3022@gmail.com>
2022-07-27 17:40:18 -0300

    fix front-end

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ def create_app(config_name):
     app = Flask(__name__)
     CORS(app)
        #Isso permite que qualquer domínio da internet acesse nossas rotas. Isso permite que um usuário faça um aplicação e fazer requisições por front-end.
-       #Além disso, pode-se passar mais argumentos. Por exemplo: resources(r'api/route')
+       #Além disso, pode-se passar mais argumentos. Por exemplo: resources(r'api/*': origins: "*"). Isso liberaria acesso apenas às rotas nossas de api e poderia ser feito por qualquer lugar (origins: "*")
     app.config.from_object(config[config_name])
 
     db.init_app(app)
